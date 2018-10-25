68fccdd5fbc12ff1680e7c45376bd9a977059eb5
achingbrain <alex@achingbrain.net>
2018-10-25 12:07:28 +0100

    adds tests and makes sure errors are not swallowed

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 
 var Source = require('pull-defer/source')
-var pull = require('pull-stream')
+var error = require('pull-stream/sources/error')
+var values = require('pull-stream/sources/values')
+var collect = require('pull-stream/sinks/collect')
 
 module.exports = function (compare) {
-
   var source = Source()
 
-  var sink = pull.collect(function (err, ary) {
-    source.resolve(pull.values(ary.sort(compare)))
+  var sink = collect(function (err, ary) {
+    if (err) {
+      return source.resolve(error(err))
+    }
+
+    source.resolve(values(ary.sort(compare)))
   })
 
   return function (read) {
     sink(read)
     return source
   }
-
 }
